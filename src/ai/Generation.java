1a4458d2ce0a2a8d3191cf38ba87bcaceb30caee
namkang321 <namkang_chutisilp@hotmail.com>
2018-11-01 16:33:18 +0700

    ADDED MUTATION_RATE and (not complete editing mutation method)

--- src/ai/Generation.java
+++ src/ai/Generation.java
@@ -7,6 +7,7 @@ import java.util.Random;
 public class Generation {
 	ArrayList<Genome> genomes = new ArrayList<Genome>();
 	ArrayList<Genome> bestGenomes = new ArrayList<Genome>();
+	final static double MUTATION_RATE = 0.25;
 	final static int NUM_BEST_GENOMES = 30; // 1 generation has 30 best genomes out of 100 genomes.
 	
 	public Generation() {
@@ -42,17 +43,15 @@ public class Generation {
 		return newGenome;
 	}
 	
-	public void mutation() {
+	public Genome mutation() {
 		Random random = new Random();
-		while(genomes.size() < 100) {
-			Genome newGenome = genomes.get(random.nextInt(NUM_BEST_GENOMES));
-			ArrayList<Double> genome = newGenome.getGenome();
-			for (int i = 0; i < random.nextInt(genome.size()); i++) {
-				genome.set(random.nextInt(genome.size()), Math.random());
-			}
-			newGenome.setGenome(genome);
-			genomes.add(newGenome);
-			}
+		Genome newGenome = genomes.get(random.nextInt(NUM_BEST_GENOMES));
+		ArrayList<Double> genome = newGenome.getGenome();
+		if(Math.random() < MUTATION_RATE) {
+			
+		}
+		newGenome.setGenome(genome);
+		return newGenome;
 	}
 	
 	public ArrayList<Genome> getBestGenomes() {
--- src/ai/Genome.java
+++ src/ai/Genome.java
@@ -29,7 +29,6 @@ public class Genome implements Comparable<Object>{
 		 * 2: Crouch
 		 */
 		this.action = random.nextInt(2);
-		
 	}
 	
 	public Genome(ArrayList<Integer> indexOfGenes, ArrayList<Double> oldGenome) { //Two parameters must have the same length
